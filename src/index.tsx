d1af7516392b8e79c08ba53466798b868954ff1a
Christopher Najafi <chris.najafi@gmail.com>
2021-08-11 16:57:33 -0400

    Added Sudoku grid and title with styling.

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ThemeProvider } from 'styled-components'
 
 import reportWebVitals from './reportWebVitals'
 import { unregister } from './core'
+import { GlobalStyles, theme } from './styles'
+import { Content, Title, Card } from './components'
+import Grid from './components/grid'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <div>Hello World</div>
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <Content data-cy="content">
+      <Title data-cy="title">Sudoku</Title>
+      <Card data-cy="card">
+        <Grid />
+      </Card>
+    </Content>
+  </ThemeProvider>,
   document.getElementById('root')
 )
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+export default {
+  colors: {
+    background: 'radial-gradient(#282C34CC, #282C34)',
+    black: '#282C34',
+    blue: '#A0E9FD',
+    lightBlue: '#CAF3FE',
+    white: 'white',
+  },
+  transition: '0.3s',
+}
